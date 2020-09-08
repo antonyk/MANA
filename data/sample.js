7a5f4532c26b5035bb8babc440708de3fcb27cc5
Anthony K <antonyk@users.noreply.github.com>
2020-09-07 23:48:32 -0400

    created sample data file

---
+++ data/sample.js
@@ -0,0 +1,51 @@
+export const frameworks = [
+  {
+    id: 1,
+    name: 'uikit',
+  },
+  {
+    id: 2,
+    name: 'bootstrap',
+  },
+]
+
+export const components = [
+  {
+    id: 1,
+    name: 'button',
+    framework_id: 1,
+  },
+  {
+    id: 2,
+    name: 'button',
+    framework_id: 2,
+  },
+]
+
+export const templates = [
+  {
+    id: 1,
+    code: '',
+    component_id: 1,
+  },
+  {
+    id: 2,
+    code: '',
+    component_id: 2,
+  },
+]
+
+export const schemas = [
+  {
+    id: 1,
+    code: '',
+    template_id: 1,
+  },
+  {
+    id: 2,
+    code: '',
+    template_id: 2,
+  },
+]
+
+export const initialTemplate = {}
