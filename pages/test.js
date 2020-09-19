6edc4a927150df5daa642f4e7369defb2a665764
Anthony K <antonyk@users.noreply.github.com>
2020-09-19 11:48:52 -0400

    Update test.js

--- pages/test.js
+++ pages/test.js
@@ -9,6 +9,70 @@ const generated = {
   expanded: 'false',
 }
 
+const buttonSettings = [
+  {
+    name: 'button-disabled',
+    type: 'checkbox',
+    class: 'mana-checkbox',
+    varname: 'is_disabled',
+    default: false,
+    current: false,
+    label: 'Disabled',
+  },
+  {
+    name: 'button-style',
+    type: 'radio',
+    items: [
+      { name: 'Default', value: '.uk-button-default' },
+      { name: 'Primary', value: '.uk-button-primary' },
+      { name: 'Secondary', value: '.uk-button-secondary' },
+      { name: 'Danger', value: '.uk-button-danger' },
+      { name: 'Text', value: '.uk-button-text' },
+      { name: 'Link', value: '.uk-button-link' },
+    ],
+    class: 'mana-radio',
+    varname: 'style',
+    default: 'Default',
+    current: 'Default',
+    label: 'Style',
+  },
+  {
+    name: 'button-size',
+    type: 'radio',
+    items: [
+      { name: 'Default', value: '' },
+      { name: 'Small', value: '.uk-button-small' },
+      { name: 'Large', value: '.uk-button-large' },
+    ],
+    class: 'mana-radio',
+    varname: 'size',
+    default: 'Default',
+    current: 'Default',
+    label: 'Size',
+  },
+]
+
+const labelSettings = [
+  {
+    name: 'button-style',
+    type: 'radio',
+    items: [
+      { name: 'Default', value: '' },
+      { name: 'Success', value: '.uk-button-success' },
+      { name: 'Warning', value: '.uk-button-warning' },
+      { name: 'Danger', value: '.uk-button-danger' },
+    ],
+    class: 'mana-radio',
+    varname: 'style',
+    default: 'Default',
+    label: 'Style',
+  },
+]
+
+const buttonTemplate = `
+    <button className="uk-button {{style}} {{size}} {{is_disabled}}"></button>
+`
+
 export default function Test() {
   return (
     <div className={styles.container}>
@@ -22,7 +86,12 @@ export default function Test() {
           Welcome to <a href='https://nextjs.org'>Next.js!</a>
         </h1>
 
-        <ComponentSettings componentId={0} schemaId={0} />
+        <ComponentSettings
+          componentId={0}
+          schemaId={0}
+          settings={buttonSettings}
+          template={buttonTemplate}
+        />
       </main>
 
       <footer className={styles.footer}>
@@ -40,25 +109,23 @@ export default function Test() {
 }
 
 function ComponentSettings(props) {
-  const [settings, setSettings] = useState(sample.schemas[props.schemaId].code)
+  const [settings, setSettings] = useState(props.settings)
   const [moustacheJson, setMoustacheJson] = useState('Initial')
 
   useEffect(() => {
-    // setMoustacheJson('')
-    // const tmpJson = JSON.parse(settings)
-    // console.log(tmpJson)
-    // const result = {}
-    // tmpJson.settings.forEach((setting) => {
-    //   result[setting.varname] = setting.current
-    // })
-    // setMoustacheJson(result)
+    setMoustacheJson('')
+    const result = {}
+    settings.forEach((setting) => {
+      result[setting.varname] = setting.current
+    })
+    setMoustacheJson(result)
   }, [settings])
 
   return (
     <>
-      <pre>{settings}</pre>
+      <pre>{JSON.stringify(settings)}</pre>
       Hello World!
-      <pre>{moustacheJson}</pre>
+      <pre>{JSON.stringify(moustacheJson)}</pre>
     </>
   )
 }
