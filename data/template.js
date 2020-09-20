fcfd1dd1487c0ff57b9afecddf7a2fa99cdf8b05
MaryamMosstoufi <maryam@zooxoos.com>
2020-09-20 12:16:02 -0400

    Schema Testing

---
+++ data/template.js
@@ -0,0 +1,270 @@
+export const schemas = [
+  {
+    framework_id: 0,
+    framework_name: "UIkit",
+    components: {
+      "Accordion": {
+        "code": [
+          <ul>
+            <li className="uk-open">
+              <a className="uk-accordion-title" href="#">Item 1</a>
+              <div className="uk-accordion-content">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              </div>
+            </li>
+            <li>
+              <a className="uk-accordion-title" href="#">Item 2</a>
+              <div className="uk-accordion-content">
+                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor reprehenderit.</p>
+              </div>
+            </li>
+            <li>
+              <a className="uk-accordion-title" href="#">Item 3</a>
+              <div className="uk-accordion-content">
+                <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat proident.</p>
+              </div>
+            </li>
+          </ul>
+        ],
+        "codeData": {
+          "required": "data-uk-accordion"
+        }
+
+      },
+      "Alert": {
+        
+      },
+      "Align": {
+        
+      },
+      "Animation": {
+        
+      },
+      "Article": {
+        
+      },
+      "Background": {
+        
+      },
+      "Badge": {
+        
+      },
+      "Base": {
+        
+      },
+      "Breadcrumb": {
+        
+      },
+      "Button": {
+        
+      },
+      "Card": {
+        
+      },
+      "Close": {
+        
+      },
+      "Column": {
+        
+      },
+      "Comment": {
+        
+      },
+      "Container": {
+        
+      },
+      "Countdown": {
+        
+      },
+      "Cover": {
+        
+      },
+      "Description List": {
+        
+      },
+      "Divider": {
+        
+      },
+      "Dotnav": {
+        
+      },
+      "Drop": {
+        
+      },
+      "Dropdown": {
+        
+      },
+      "Filter": {
+        
+      },
+      "Flex": {
+        
+      },
+      "Form": {
+        
+      },
+      "Grid": {
+        
+      },
+      "Heading": {
+        
+      },
+      "Height": {
+        
+      },
+      "Icon": {
+        
+      },
+      "Iconnav": {
+        
+      },
+      "Image": {
+        
+      },
+      "Inverse": {
+        
+      },
+      "Label": {
+        
+      },
+      "Leader": {
+        
+      },
+      "Lightbox": {
+        
+      },
+      "Link": {
+        
+      },
+      "List": {
+        
+      },
+      "Margin": {
+        
+      },
+      "Marker": {
+        
+      },
+      "Modal": {
+        
+      },
+      "Nav": {
+        
+      },
+      "Navbar": {
+        
+      },
+      "Notification": {
+        
+      },
+      "Off-canvas": {
+        
+      },
+      "Overlay": {
+        
+      },
+      "Padding": {
+        
+      },
+      "Pagination": {
+        
+      },
+      "Parallax": {
+        
+      },
+      "Placeholder": {
+        
+      },
+      "Position": {
+        
+      },
+      "Print": {
+        
+      },
+      "Progress": {
+        
+      },
+      "Scroll": {
+        
+      },
+      "Scrollspy": {
+        
+      },
+      "Search": {
+        
+      },
+      "Section": {
+        
+      },
+      "Slidenav": {
+        
+      },
+      "Slider": {
+        
+      },
+      "Slideshow": {
+        
+      },
+      "Sortable": {
+        
+      },
+      "Spinner": {
+        
+      },
+      "Sticky": {
+        
+      },
+      "Subnav": {
+        
+      },
+      "SVG": {
+        
+      },
+      "Switcher": {
+        
+      },
+      "Tab": {
+        
+      },
+      "Table": {
+        
+      },
+      "Text": {
+        
+      },
+      "Thumbnav": {
+        
+      },
+      "Tile": {
+        
+      },
+      "Toggle": {
+        
+      },
+      "Tooltip": {
+        
+      },
+      "Totop": {
+        
+      },
+      "Transition": {
+        
+      },
+      "Upload": {
+        
+      },
+      "Utility": {
+        
+      },
+      "Video": {
+        
+      },
+      "Visibility": {
+        
+      },
+      "Width": {
+        
+      },
+    },
+  }
+]
--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,6 @@ import '../node_modules/uikit/dist/js/uikit.min';
 import '../node_modules/uikit/dist/js/uikit-icons.min';
 import '../styles/globals.css';
 
-
-
-
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
