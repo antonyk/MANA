76ca485a1f5b59c19a572c83cfdde35d1cccd3db
MaryamMosstoufi <maryam@zooxoos.com>
2020-09-19 21:19:37 -0400

    Sidebar Framework Select

--- src/components/app/SidebarMenu.js
+++ src/components/app/SidebarMenu.js
@@ -5,24 +5,32 @@ export default function SidebarMenu() {
   const [frameworkID, setFrameworkID] = useState(null);
 
   const handleClick = (event) => {
-    setFrameworkID(event.target.id);
+    setFrameworkID(event.target.value);
   };
 
   return (
     <>
-      <ul class="uk-nav uk-nav-default">
-        {frameworks.map((item) => (
+      <ul className="uk-nav uk-nav-default">
+        {/* {frameworks.map((item) => (
           <h4
             id={item.framework_id}
             onClick={handleClick}
           >{`${item.framework_name}`}</h4>
-        ))}
-      </ul>
+        ))} */}
+        <select className="uk-select uk-margin-bottom"  onChange={handleClick}>
+          {frameworks.map((item) => (
+            <option
+            value={item.framework_id}
+          >{`${item.framework_name}`}</option>
+          ))}
+        </select>
+      
       {frameworkID
         ? frameworks[frameworkID].components
             .sort()
             .map((item) => <li>{`${item}`}</li>)
-        : "Select a framework to get started"}
+          : "Select a framework to get started"}
+      </ul>  
     </>
   );
 }
