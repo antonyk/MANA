b817c5b70d654850a003417f053e1c24c23b2e97
MaryamMosstoufi <maryam@zooxoos.com>
2020-09-20 00:31:09 -0400

    Custom Syntax Highlighter

--- src/components/app/Editor.js
+++ src/components/app/Editor.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import useClipboard from 'react-use-clipboard'
 import SyntaxHighlighter from 'react-syntax-highlighter'
+import {docco} from 'react-syntax-highlighter/dist/cjs/styles/hljs';
+// import syntax from '../app/Syntax'
 
 export default function Template() {
   
@@ -23,23 +25,24 @@ export default function Template() {
     <>
       <div className='uk-section uk-section-small'>
         <div className='uk-container'>
-          <h2 className=''>UIKit Button</h2>
+          <h2 className='uk-text-center uk-text-primary'>UIKit Button</h2>
           <div id='preview' className='uk-card uk-card-body uk-card-default uk-margin-bottom' >
+            <h3 className='uk-card-title'>Preview</h3>
             <div className='uk-tile uk-tile-muted'>
               {code[0]}
             </div>
           </div>
           <div id='settings' className='uk-card uk-card-default uk-card-body uk-margin-bottom'>
-            <h3 className='uk-card-title uk-text-uppercase'>Settings</h3>
+            <h3 className='uk-card-title'>Settings</h3>
             {/* settings go here */}
           </div>
           <div id='code' className='uk-card uk-card-default uk-card-body uk-margin-bottom'>
-            <h3 className='uk-card-title uk-text-uppercase uk-display-inline'>Code</h3>
+            <h3 className='uk-card-title uk-display-inline'>Code</h3>
             <a onClick={setCopied} data-uk-tooltip='Copy To Clipboard!' className='uk-float-right'> 
                 <i className='fad fa-copy fa-lg'></i>
                 {/* {isCopied ? 'Yes! 👍' : 'Nope! 👎'} */}
             </a>
-            <SyntaxHighlighter language='javascript'>
+            <SyntaxHighlighter language='javascript' style={docco}>
               {codeString}
             </SyntaxHighlighter>
           </div>
