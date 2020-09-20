560893a6e92d67e52fee8645e8c18ad3f9d2eb51
Alex Kemper <alex.j.kemper@gmail.com>
2020-09-19 20:59:04 -0400

    sidemenu.js functionality

--- src/components/app/SidebarMenu.js
+++ src/components/app/SidebarMenu.js
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useState } from "react";
+import { frameworks } from "../../../data/framework_data";
 
 export default function SidebarMenu() {
+  const [frameworkID, setFrameworkID] = useState(null);
+
+  const handleClick = (event) => {
+    setFrameworkID(event.target.id);
+  };
+
   return (
     <>
-      <h3>Title</h3>
       <ul class="uk-nav uk-nav-default">
-        {/* map all components in the framework and return a link */}
-        <li><a href="#">Active</a></li>
+        {frameworks.map((item) => (
+          <h4
+            id={item.framework_id}
+            onClick={handleClick}
+          >{`${item.framework_name}`}</h4>
+        ))}
       </ul>
+      {frameworkID
+        ? frameworks[frameworkID].components
+            .sort()
+            .map((item) => <li>{`${item}`}</li>)
+        : "Select a framework to get started"}
     </>
-  )
+  );
 }
