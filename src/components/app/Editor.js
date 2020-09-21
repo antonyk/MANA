043bd7e75cb5c48686184aec0c464a09b2d1020b
Anthony K <antonyk@users.noreply.github.com>
2020-09-21 09:42:17 -0400

    prettified js file

--- src/components/app/Editor.js
+++ src/components/app/Editor.js
@@ -1,67 +1,67 @@
-import React from "react";
-import useClipboard from "react-use-clipboard";
-import SyntaxHighlighter from "react-syntax-highlighter";
-import syntax from "../app/Syntax";
+import React from 'react'
+import useClipboard from 'react-use-clipboard'
+import SyntaxHighlighter from 'react-syntax-highlighter'
+import syntax from './Syntax'
 
 export default function Template() {
-  let codeStringClass = `'uk-button uk-button-secondary'`;
-  let codeClass = `uk-button uk-button-secondary`;
-  let codeData = `disabled`;
+  let codeStringClass = `'uk-button uk-button-secondary'`
+  let codeClass = `uk-button uk-button-secondary`
+  let codeData = `disabled`
   let codeString = `
   <div>
     <button className=${codeStringClass} ${codeData}>Button</button>
   </div>
-  `;
+  `
   let code = [
     <div>
       <button className={codeClass}>Button</button>
     </div>,
-  ];
-  let [isCopied, setCopied] = useClipboard(codeString);
+  ]
+  let [isCopied, setCopied] = useClipboard(codeString)
 
   return (
     <>
-      <div className="uk-section uk-section-small">
-        <div className="uk-container">
-          <h2 className="uk-text-center">UIKit Button</h2>
+      <div className='uk-section uk-section-small'>
+        <div className='uk-container'>
+          <h2 className='uk-text-center'>UIKit Button</h2>
           <div
-            id="preview"
-            className="uk-card uk-card-body uk-card-default uk-margin-bottom"
+            id='preview'
+            className='uk-card uk-card-body uk-card-default uk-margin-bottom'
           >
-            <h3 className="uk-card-title">Preview</h3>
-            <div className="uk-tile uk-tile-muted">{code[0]}</div>
+            <h3 className='uk-card-title'>Preview</h3>
+            <div className='uk-tile uk-tile-muted'>{code[0]}</div>
           </div>
           <div
-            id="settings"
-            className="uk-card uk-card-default uk-card-body uk-margin-bottom"
+            id='settings'
+            className='uk-card uk-card-default uk-card-body uk-margin-bottom'
           >
-            <h3 className="uk-card-title">Settings</h3>
+            <h3 className='uk-card-title'>Settings</h3>
             {/* settings go here */}
           </div>
           <div
-            id="code"
-            className="uk-card uk-card-default uk-card-body uk-margin-bottom"
+            id='code'
+            className='uk-card uk-card-default uk-card-body uk-margin-bottom'
           >
-            <h3 className="uk-card-title uk-display-inline">Code</h3>
+            <h3 className='uk-card-title uk-display-inline'>Code</h3>
             <a
               onClick={setCopied}
-              data-uk-tooltip="Copy To Clipboard!"
-              className="uk-float-right"
+              data-uk-tooltip='Copy To Clipboard!'
+              className='uk-float-right'
             >
-              <i className="fad fa-copy fa-lg"></i>
+              <i className='fad fa-copy fa-lg'></i>
               {/* {isCopied ? 'Yes! 👍' : 'Nope! 👎'} */}
             </a>
-            <SyntaxHighlighter language="javascript" style={syntax}>
+            <SyntaxHighlighter language='javascript' style={syntax}>
               {codeString}
             </SyntaxHighlighter>
           </div>
-          <div className="uk-text-center">
-            <a className="uk-text-center" href="">
+          <div className='uk-text-center'>
+            <a className='uk-text-center' href=''>
               UIKit Documentation
             </a>
           </div>
         </div>
       </div>
     </>
-  );
+  )
 }
